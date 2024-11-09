37196a43000f61e390726538914aa22c476f9d3b
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-09 15:00:43 +0300

    security config

--- src/main/java/com/slippery/jobposts/config/CorsConfig.java
+++ src/main/java/com/slippery/jobposts/config/CorsConfig.java
@@ -1,4 +1,19 @@
 package com.slippery.jobposts.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://book-store-ui-ten.vercel.app")
+                        .allowedMethods("GET","DELETE","POST","PUT");
+            }
+        };
+    }
 }
